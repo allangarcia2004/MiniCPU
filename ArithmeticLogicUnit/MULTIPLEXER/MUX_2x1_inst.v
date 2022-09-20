3f9cf6b566c2b5610da056b275cf63df6f9e0332
Allan Garcia Cavalcante e Silva <allangarcia@usp.br>
2022-09-20 17:39:19 -0300

    .

---
+++ ArithmeticLogicUnit/MULTIPLEXER/MUX_2x1_inst.v
@@ -0,0 +1,6 @@
+MUX_2x1	MUX_2x1_inst (
+	.data0x ( data0x_sig ),
+	.data1x ( data1x_sig ),
+	.sel ( sel_sig ),
+	.result ( result_sig )
+	);
---
+++ Memory/ROM_16x256_inst.v
@@ -0,0 +1,5 @@
+ROM_16x256	ROM_16x256_inst (
+	.address ( address_sig ),
+	.clock ( clock_sig ),
+	.q ( q_sig )
+	);
